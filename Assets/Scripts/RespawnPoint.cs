d44dd0527c1d94807739c7c1ce8e0951ab24b9fc
Otju <ottoj.soderman@gmail.com>
2026-04-25 22:04:54 +0300

    Improve respawn

--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class RespawnPoint : MonoBehaviour
 {
+    [Header("Editor Gizmo")]
+    [SerializeField] Color gizmoColor = new(0.2f, 0.9f, 0.3f, 0.9f);
+    [SerializeField] float gizmoSizeMultiplier = 0.05f;
+    [SerializeField] bool drawDirectionArrow = true;
+    [SerializeField] float arrowLengthMultiplier = 0.5f;
+
     void Awake()
     {
         var ownCollider = GetComponent<Collider>();
@@ -15,7 +21,7 @@ public class RespawnPoint : MonoBehaviour
             return;
         var helicopterController = helicopterRoot.GetComponent<HelicopterController>();
         if (helicopterController != null)
-            helicopterController.SetLastRespawnPoint(GetCenterWorldPosition(), transform.rotation);
+            helicopterController.SetLastRespawnPoint(GetCenterWorldPosition(), GetRespawnRotation());
         Debug.Log($"RespawnPoint hit by helicopter '{helicopterRoot.name}'.", this);
     }
 
@@ -25,6 +31,58 @@ public class RespawnPoint : MonoBehaviour
         if (ownCollider != null)
             return ownCollider.bounds.center;
         return transform.position;
+    }
+
+    void OnDrawGizmos()
+    {
+        DrawRespawnGizmo(false);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        DrawRespawnGizmo(true);
+    }
+
+    void DrawRespawnGizmo(bool isSelected)
+    {
+        Gizmos.color = isSelected ? Color.yellow : gizmoColor;
+        Vector3 center = GetCenterWorldPosition();
+        float referenceSize = GetReferenceSize();
+        float gizmoRadius = referenceSize * gizmoSizeMultiplier;
+        float arrowLength = referenceSize * arrowLengthMultiplier;
+        Vector3 normal = GetRespawnNormal();
+
+        Gizmos.DrawSphere(center, gizmoRadius);
+
+        if (!drawDirectionArrow)
+            return;
+
+        Vector3 tip = center + normal * arrowLength;
+        Gizmos.DrawLine(center, tip);
+        Gizmos.DrawSphere(tip, gizmoRadius * 0.35f);
+    }
+
+    float GetReferenceSize()
+    {
+        var ownCollider = GetComponent<Collider>();
+        if (ownCollider == null)
+            return 1f;
+        float size = ownCollider.bounds.extents.magnitude;
+        return Mathf.Max(0.5f, size);
+    }
+
+    Vector3 GetRespawnNormal()
+    {
+        Vector3 normal = transform.up;
+        if (normal.sqrMagnitude < 1e-6f)
+            return Vector3.forward;
+        return normal.normalized;
+    }
+
+    Quaternion GetRespawnRotation()
+    {
+        Vector3 normal = GetRespawnNormal();
+        return Quaternion.FromToRotation(Vector3.forward, normal);
     }
 
     static bool TryGetHelicopterRoot(Collider other, out Transform root)
